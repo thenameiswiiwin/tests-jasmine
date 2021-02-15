c5b59624c4548d55b5e7869a808e25b6111dcdca
thenameiswiiwin <huyn.nguyen95@gmail.com>
2021-02-15 16:09:58 -0500

    final commit: tests passed

--- src/test.js
+++ src/test.js
@@ -1,50 +1,83 @@
 function addTwoToEveryNumberInTheList(list) {
-	// an example for you to see how the tests work
-	const newList = list.map((city) => {
-		newList.push(list[i]);
-	});
+	const newList = [];
+
+	for (let i = 0; i < list.length; i++) {
+		newList.push(list[i] + 2);
+	}
+
+	return newList;
 }
 
 function findLargestNumber(list) {
-	// return the largest number in array
+	let largest = list[0];
 
-	return 0;
+	for (let i = 0; i < list.length; i++) {
+		if (list[i] > largest) {
+			largest = list[i];
+		}
+	}
+
+	return largest;
 }
 
 function makeDogObject() {
-	// return a dog object that
-	//   has a speak method that returns woof
-	//   has a name property of "Fido"
-	//   has a color property of "white"
-	//   has an age property of 6
-
-	return {};
+	return {
+		name: 'Fido',
+		color: 'white',
+		age: 6,
+		speak() {
+			return 'woof';
+		},
+	};
 }
 
 function getListOfNames(list) {
-	// list is an array of objects that looks like this
-	// { name: "Some Person", jobTitle: "Boss Person", age: 30 }
-	// return a list of all the workers' names
+	const names = [];
 
-	return ['Bobby Baratheon', 'Jonny Snow', 'Dani Targaryen'];
+	for (let i = 0; i < list.length; i++) {
+		names.push(list[i].name);
+	}
+	return names;
 }
 
 function getAverageAge(list) {
-	// using the same list as above, get the average age of all the workers'
+	let sum = 0;
 
-	return 15;
+	for (let i = 0; i < list.length; i++) {
+		sum += list[i].age;
+	}
+
+	return sum / list.length;
 }
 
 function getAllEngineers(list) {
 	// using the same list as above, return a list of all people who have the word "Engineer" in their jobTitle
 
-	return ['Altaïr', 'Ezio', 'Connor', 'Bayek', 'Alexios'];
+	const names = [];
+
+	for (let i = 0; i < list.length; i++) {
+		if (list[i].jobTitle.includes('Engineer')) {
+			names.push(list[i].name);
+		}
+	}
+
+	return names;
 }
 
 function sort(list) {
-	// sort a list of numbers into ascending order
-	// if you need more help, Google how to do bubble sort
-	// you can sort, if you want to write it by hand, use bubble sort or insertion sort
+	let swapped = true;
 
-	return [1, 2, 3, 4];
+	while (swapped) {
+		swapped = false;
+
+		for (let i = 0; i < list.length; i++) {
+			if (list[i] > list[i + 1]) {
+				const temp = list[i];
+				list[i] = list[i + 1];
+				list[i + 1] = temp;
+				swapped = true;
+			}
+		}
+	}
+	return list;
 }
