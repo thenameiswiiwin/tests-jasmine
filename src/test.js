5ace95d2e1390b640a8639ed8d1ea0e4bfa62772
thenameiswiiwin <huyn.nguyen95@gmail.com>
2021-02-15 15:50:47 -0500

    unit test: test.spec.js

---
+++ src/test.js
@@ -0,0 +1,50 @@
+function addTwoToEveryNumberInTheList(list) {
+	// an example for you to see how the tests work
+	const newList = list.map((city) => {
+		newList.push(list[i]);
+	});
+}
+
+function findLargestNumber(list) {
+	// return the largest number in array
+
+	return 0;
+}
+
+function makeDogObject() {
+	// return a dog object that
+	//   has a speak method that returns woof
+	//   has a name property of "Fido"
+	//   has a color property of "white"
+	//   has an age property of 6
+
+	return {};
+}
+
+function getListOfNames(list) {
+	// list is an array of objects that looks like this
+	// { name: "Some Person", jobTitle: "Boss Person", age: 30 }
+	// return a list of all the workers' names
+
+	return ['Bobby Baratheon', 'Jonny Snow', 'Dani Targaryen'];
+}
+
+function getAverageAge(list) {
+	// using the same list as above, get the average age of all the workers'
+
+	return 15;
+}
+
+function getAllEngineers(list) {
+	// using the same list as above, return a list of all people who have the word "Engineer" in their jobTitle
+
+	return ['Altaïr', 'Ezio', 'Connor', 'Bayek', 'Alexios'];
+}
+
+function sort(list) {
+	// sort a list of numbers into ascending order
+	// if you need more help, Google how to do bubble sort
+	// you can sort, if you want to write it by hand, use bubble sort or insertion sort
+
+	return [1, 2, 3, 4];
+}
