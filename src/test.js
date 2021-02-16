14ac41d8fe88c8aac0621d41d83491a73a192b5c
thenameiswiiwin <huyn.nguyen95@gmail.com>
2021-02-16 16:50:29 -0500

    JS: refactored. Final Commit

--- src/test.js
+++ src/test.js
@@ -1,83 +1,42 @@
 function addTwoToEveryNumberInTheList(list) {
-	const newList = [];
-
-	for (let i = 0; i < list.length; i++) {
-		newList.push(list[i] + 2);
-	}
-
+	const newList = list.map((list) => list + 2);
 	return newList;
 }
 
 function findLargestNumber(list) {
-	let largest = list[0];
-
-	for (let i = 0; i < list.length; i++) {
-		if (list[i] > largest) {
-			largest = list[i];
-		}
-	}
-
+	const largest = Math.max(...list);
 	return largest;
 }
 
 function makeDogObject() {
 	return {
-		name: 'Fido',
-		color: 'white',
-		age: 6,
 		speak() {
 			return 'woof';
 		},
+		name: 'Fido',
+		color: 'white',
+		age: 6,
 	};
 }
 
 function getListOfNames(list) {
-	const names = [];
-
-	for (let i = 0; i < list.length; i++) {
-		names.push(list[i].name);
-	}
+	const names = list.map(({ name }) => name);
 	return names;
 }
 
 function getAverageAge(list) {
-	let sum = 0;
-
-	for (let i = 0; i < list.length; i++) {
-		sum += list[i].age;
-	}
-
+	const sum = list.reduce((a, b) => a + b.age, 0);
 	return sum / list.length;
 }
 
 function getAllEngineers(list) {
-	// using the same list as above, return a list of all people who have the word "Engineer" in their jobTitle
-
-	const names = [];
-
-	for (let i = 0; i < list.length; i++) {
-		if (list[i].jobTitle.includes('Engineer')) {
-			names.push(list[i].name);
-		}
-	}
-
-	return names;
+	const engineers = list
+		.filter(({ jobTitle }) => jobTitle.includes('Engineer'))
+		.map(({ name }) => name);
+	return engineers;
 }
 
 function sort(list) {
-	let swapped = true;
-
-	while (swapped) {
-		swapped = false;
-
-		for (let i = 0; i < list.length; i++) {
-			if (list[i] > list[i + 1]) {
-				const temp = list[i];
-				list[i] = list[i + 1];
-				list[i + 1] = temp;
-				swapped = true;
-			}
-		}
-	}
-	return list;
+	const sortedList = list.sort((a, b) => a - b);
+	return sortedList;
 }
